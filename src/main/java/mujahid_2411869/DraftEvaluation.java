a8049cae56c9eea99b244cc71e194dac0b1aea28
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-17 18:54:50 +0600

    Added new class DraftEvaluation

---
+++ src/main/java/mujahid_2411869/DraftEvaluation.java
@@ -0,0 +1,31 @@
+package mujahid_2411869;
+
+import java.time.LocalDate;
+
+public class DraftEvaluation {
+    private String evaluationID; // {unique}
+    private String draftID;
+    private String reviewDecision;
+    private String executiveComments;
+    private LocalDate dateEvaluated;
+
+    public boolean evaluateDraft(String draftID, String decision, String comments) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean changeEvaluationCacheLogState() {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean testConditionalDependency(String decision, String comments) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean evaluateRejectedStateRules(String decision, String comments) {
+        // TODO: Implement logic
+        return false;
+    }
+}
