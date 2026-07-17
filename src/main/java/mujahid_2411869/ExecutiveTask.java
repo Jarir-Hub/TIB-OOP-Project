04e39ae2af6ad3530c21d51788ee35dd1b2e6c9a
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-17 18:57:37 +0600

    Added new class ExecutiveTask

---
+++ src/main/java/mujahid_2411869/ExecutiveTask.java
@@ -0,0 +1,32 @@
+package mujahid_2411869;
+
+import java.time.LocalDate;
+
+public class ExecutiveTask {
+    private String taskID; // {unique}
+    private String taskTitle;
+    private String priorityLevel;
+    private LocalDate dueDate;
+    private String taskNotes;
+    private String status;
+
+    public boolean createPersonalTask(String title, String priority, LocalDate dueDate, String notes) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean logTaskToTable() {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean validateTitlePresence(String title) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean validateDropdownSelection(String priority) {
+        // TODO: Implement logic
+        return false;
+    }
+}
