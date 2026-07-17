b8b73ac6dc3af7e4ef931a1118e2643885d0961d
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-17 18:46:49 +0600

    Added StrategicRoadmap Class

---
+++ src/main/java/mujahid_2411869/StrategicRoadmap.java
@@ -0,0 +1,40 @@
+package mujahid_2411869;
+
+import java.time.LocalDate;
+
+public class StrategicRoadmap {
+    private String roadmapID; // {unique}
+    private String roadmapFiscalYear;
+    private String focusArea;
+    private String keyMilestones;
+    private LocalDate targetCompletionDate;
+    private String status = "Finalized";
+
+
+    public boolean defineRoadmap(String fiscalYear, String focusArea, String milestones, LocalDate targetDate) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean injectMetadataToDB() {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean testYearRestrictions(String fiscalYear) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean testStructuralStringCompletion(String milestones) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean testStructuralStringCompletion() {
+        // TODO: Implement logic
+        return false;
+    }
+}
+
+
