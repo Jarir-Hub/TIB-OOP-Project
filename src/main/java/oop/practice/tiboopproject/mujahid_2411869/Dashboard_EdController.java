4a904ad989df09534e79d87277a09bfd7e0a166b
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-19 13:04:30 +0600

    Added Ed_dashboad_Fxml

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/Dashboard_EdController.java
@@ -0,0 +1,44 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class Dashboard_EdController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void ReviewAndApproveOrRejectAResearchDraftButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CreateAndScheduleExecutiveMeetingAgendaButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CreateAndManageOrganisationalPoliciesButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ManagePersonalExecutiveTasksButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FinalizeAnnualStrategicRoadmapButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReleaseOfficialPressStatementsButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SignStrategicInstitutionalMoUsButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SendAnOrganizationalAnnouncementButtonOA(ActionEvent actionEvent) throws IOException {
+    }
+}
---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/organizationalAnnouncement_EDController.java
@@ -0,0 +1,9 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+public class organizationalAnnouncement_EDController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+
+    }
+    }
