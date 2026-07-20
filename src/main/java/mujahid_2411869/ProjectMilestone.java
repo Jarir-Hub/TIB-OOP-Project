994bd94e86b2bd7c3514c89dcd707a5ef0a80bfc
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-20 20:55:19 +0600

    Added Project Milestone Class

---
+++ src/main/java/mujahid_2411869/ProjectMilestone.java
@@ -0,0 +1,27 @@
+package mujahid_2411869;
+
+import java.time.LocalDate;
+
+public class ProjectMilestone {
+    private String milestoneID; // {unique}
+    private String associatedProject;
+    private String milestoneName;
+    private String milestoneDescription;
+    private LocalDate targetDeadline;
+    private String completionStatus;
+
+    public boolean validateDateLogic(LocalDate date) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean validateNonNullTitle(String title) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public String addMilestoneTrackingLogic() {
+        // TODO: Implement logic
+        return "";
+    }
+}
