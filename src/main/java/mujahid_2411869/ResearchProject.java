6683e951399af6b16eb0026b7411281d2100b1f9
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-20 20:56:38 +0600

    Added Research Project class

---
+++ src/main/java/mujahid_2411869/ResearchProject.java
@@ -0,0 +1,28 @@
+package mujahid_2411869;
+
+import java.time.LocalDate;
+
+public class ResearchProject {
+    private String projectID; // {unique}
+    private String projectTitle;
+    private String sector;
+    private LocalDate startDate;
+    private double estimatedBudget;
+    private String leadResearcher;
+    private String status;
+
+    public boolean checkTitleNotEmpty(String title) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public boolean validatePositiveBudget(double budget) {
+        // TODO: Implement logic
+        return false;
+    }
+
+    public String commitProjectToDB() {
+        // TODO: Implement logic
+        return "";
+    }
+}
