59a271d6f69367fd9e4abda9ef3083e0a67c85bd
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-25 01:25:38 +0600

    Added new task and executive task dashboard fxml

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/AddNewTaskViewED.java
@@ -0,0 +1,33 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddNewTaskViewED
+{
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private ComboBox priorityLevelCMB;
+    @javafx.fxml.FXML
+    private TextField taskTitleTF;
+    @javafx.fxml.FXML
+    private TextField taskNotesTF;
+    @javafx.fxml.FXML
+    private DatePicker dueDateDP;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveTaskButtonOA(ActionEvent actionEvent) {
+    }
+}
