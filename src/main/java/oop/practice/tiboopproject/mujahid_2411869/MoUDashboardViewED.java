7320fa5c6c8cafdf9757f6b97699b5ede7d18098
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-24 20:49:41 +0600

    Added MoU Dashboard

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/MoUDashboardViewED.java
@@ -0,0 +1,35 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MoUDashboardViewED
+{
+    @javafx.fxml.FXML
+    private TableView mouTV;
+    @javafx.fxml.FXML
+    private TableColumn domainTC;
+    @javafx.fxml.FXML
+    private TableColumn durationTC;
+    @javafx.fxml.FXML
+    private TableColumn partnerNameTC;
+    @javafx.fxml.FXML
+    private TableColumn expiaryDateTC;
+    @javafx.fxml.FXML
+    private TableColumn statusTC;
+    @javafx.fxml.FXML
+    private TableColumn mouIdTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerNewMoUButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+}
