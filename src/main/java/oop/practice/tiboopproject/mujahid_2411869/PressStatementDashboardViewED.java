87b5452a271d650c12021a227436dba26b888c7f
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-24 23:43:30 +0600

    Added Press Statement Dashboard

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/PressStatementDashboardViewED.java
@@ -0,0 +1,33 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PressStatementDashboardViewED
+{
+    @javafx.fxml.FXML
+    private TableColumn statementIdTC;
+    @javafx.fxml.FXML
+    private TableColumn authorTC;
+    @javafx.fxml.FXML
+    private TableView pressStatementTV;
+    @javafx.fxml.FXML
+    private TableColumn titleTC;
+    @javafx.fxml.FXML
+    private TableColumn associatedIssueTC;
+    @javafx.fxml.FXML
+    private TableColumn dateReleasedTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createPressStatementButtonOA(ActionEvent actionEvent) {
+    }
+}
