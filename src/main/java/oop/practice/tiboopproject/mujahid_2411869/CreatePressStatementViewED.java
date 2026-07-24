1bca86d2c543efc82607e099be46c485c9bea445
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-25 00:03:20 +0600

    Added Create Press Statemnt

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/CreatePressStatementViewED.java
@@ -0,0 +1,32 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CreatePressStatementViewED
+{
+    @javafx.fxml.FXML
+    private DatePicker releasedDateDP;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private TextField pressTitleTF;
+    @javafx.fxml.FXML
+    private TextField associatedIssueTF;
+    @javafx.fxml.FXML
+    private TextField statementTextTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void broadcastStatementButtonOA(ActionEvent actionEvent) {
+    }
+}
