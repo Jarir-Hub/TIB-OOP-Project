a83ce9ff82fe5ab89e1cc91f775acc590bef72d6
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-24 20:38:37 +0600

    Added Policy Creation

--- src/main/java/oop/practice/tiboopproject/mujahid_2411869/PolicyCreationViewED.java
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/PolicyCreationViewED.java
@@ -2,31 +2,32 @@ package oop.practice.tiboopproject.mujahid_2411869;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class AnnouncementCreationViewED
+public class PolicyCreationViewED
 {
     @javafx.fxml.FXML
-    private ComboBox targetAudienceCMB;
+    private ComboBox categoryCMB;
     @javafx.fxml.FXML
-    private TextField messageBodyTF;
+    private TextField policyNameTF;
     @javafx.fxml.FXML
-    private TextField announcementTitleTF;
-    @javafx.fxml.FXML
-    private TextField authorNameTF;
+    private TextField policyDescriptionTF;
     @javafx.fxml.FXML
     private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private DatePicker effectiveDateDP;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
     @javafx.fxml.FXML
-    public void sendAnnouncementButtonOA(ActionEvent actionEvent) {
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
-    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    public void publishPolicyButtonOA(ActionEvent actionEvent) {
     }
 }
