6a9e4b471083af9dedce961f0a5b8f47b3478994
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-25 00:30:46 +0600

    Added Strategic Roadmap FXML and Controllers

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/NewRoadmapViewED.java
@@ -0,0 +1,33 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class NewRoadmapViewED
+{
+    @javafx.fxml.FXML
+    private TextField jeyMilestonesTF;
+    @javafx.fxml.FXML
+    private TextField focusAreaTF;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private ComboBox fiscalYearCMB;
+    @javafx.fxml.FXML
+    private DatePicker completionDateDP;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void finalizeRoadmapButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/StrategicRoadmapDashboardViewED.java
@@ -0,0 +1,33 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class StrategicRoadmapDashboardViewED
+{
+    @javafx.fxml.FXML
+    private TableView strategyTV;
+    @javafx.fxml.FXML
+    private TableColumn focusAreaTC;
+    @javafx.fxml.FXML
+    private TableColumn fiscalYearTC;
+    @javafx.fxml.FXML
+    private TableColumn statusTC;
+    @javafx.fxml.FXML
+    private TableColumn roadmapIdTC;
+    @javafx.fxml.FXML
+    private TableColumn targetDateTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void defineNewRoadmapButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+}
