60d2d47cf4ce42f0837dbf6af98eabcccce23679
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-24 20:22:34 +0600

    Added Policy Dashboard

--- src/main/java/oop/practice/tiboopproject/mujahid_2411869/ScheduleNewMeetingViewED.java
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/ScheduleNewMeetingViewED.java
@@ -1,7 +1,33 @@
 package oop.practice.tiboopproject.mujahid_2411869;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class ScheduleNewMeetingViewED
 {
     @javafx.fxml.FXML
+    private DatePicker meetingDateDP;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private TextField meetingTitleTF;
+    @javafx.fxml.FXML
+    private ComboBox participantsCMB;
+    @javafx.fxml.FXML
+    private TextField agendaPointsTF;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveMeetingButtonOA(ActionEvent actionEvent) {
+    }
+}
