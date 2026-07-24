125fbe741dcb9092eff7bcf90c8d3cb0b1bd4661
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-24 20:59:56 +0600

    Added register new  MoU

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/RegisterNewMoUViewED.java
@@ -0,0 +1,33 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RegisterNewMoUViewED
+{
+    @javafx.fxml.FXML
+    private TextField durationTF;
+    @javafx.fxml.FXML
+    private ComboBox mouDomainCMB;
+    @javafx.fxml.FXML
+    private TextField partnerOrganizationNameTF;
+    @javafx.fxml.FXML
+    private DatePicker expiaryDateDP;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerNewMoUButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void executiveDirectoryButtonOA(ActionEvent actionEvent) {
+    }
+}
