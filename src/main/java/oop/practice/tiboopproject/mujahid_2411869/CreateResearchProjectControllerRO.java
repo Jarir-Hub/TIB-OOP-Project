fb9816c3277b77603ad0e392f9984814c002bc01
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-27 00:32:33 +0600

    Added ResearchCreation Controller and Fxml

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/CreateResearchProjectControllerRO.java
@@ -0,0 +1,35 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CreateResearchProjectControllerRO
+{
+    @javafx.fxml.FXML
+    private TextField projectTitleTF;
+    @javafx.fxml.FXML
+    private ComboBox sectorCMB;
+    @javafx.fxml.FXML
+    private TextField leadResearcherTF;
+    @javafx.fxml.FXML
+    private TextField estimatedBudgetTF;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private DatePicker startDateDP;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void researchOperationsHubButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createProjectButtonOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/ResearchDashboardControllerRO.java
@@ -0,0 +1,35 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ResearchDashboardControllerRO
+{
+    @javafx.fxml.FXML
+    private TableColumn estimatedBudgetTC;
+    @javafx.fxml.FXML
+    private TableView researchTV;
+    @javafx.fxml.FXML
+    private TableColumn projectIdTC;
+    @javafx.fxml.FXML
+    private TableColumn sectorTC;
+    @javafx.fxml.FXML
+    private TableColumn statusTC;
+    @javafx.fxml.FXML
+    private TableColumn projectTitleTC;
+    @javafx.fxml.FXML
+    private TableColumn startDateTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void researchOperationsHubButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void startBewProjectButtonOA(ActionEvent actionEvent) {
+    }
+}
