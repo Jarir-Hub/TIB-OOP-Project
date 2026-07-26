cbd5f9be3b1e239e4e59113efa499fa92a852471
Mujahid-Maruf <2411869@iub.edu.bd>
2026-07-27 00:57:43 +0600

    Added Research Draft Info

---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/DraftDashboardControllerRO.java
@@ -0,0 +1,33 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class DraftDashboardControllerRO
+{
+    @javafx.fxml.FXML
+    private TableColumn parentProjectTitleTC;
+    @javafx.fxml.FXML
+    private TableColumn submissionDateTC;
+    @javafx.fxml.FXML
+    private TableColumn statusTC;
+    @javafx.fxml.FXML
+    private TableColumn draftIdTC;
+    @javafx.fxml.FXML
+    private TableColumn draftTitleTC;
+    @javafx.fxml.FXML
+    private TableView draftTV;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void researchOperationsHubButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitNewDraftButtonOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/oop/practice/tiboopproject/mujahid_2411869/SubmitNewDraftControllerRO.java
@@ -0,0 +1,32 @@
+package oop.practice.tiboopproject.mujahid_2411869;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SubmitNewDraftControllerRO
+{
+    @javafx.fxml.FXML
+    private TextField draftTitleTF;
+    @javafx.fxml.FXML
+    private TextField summaryTF;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private TextField attachmentLinkTF;
+    @javafx.fxml.FXML
+    private ComboBox selectProjectCMB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void researchOperationsHubButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitForApprovalButtonOA(ActionEvent actionEvent) {
+    }
+}
