9b01d6ea1c06a82faab291b3e7ba4c5dd41776c1
mohonaaa2431026 <2431026@iub.edu.bd>
2026-08-07 10:43:28 +0600

    h

--- src/main/java/mohona_2431026/ViewComplaintStatus.java
+++ src/main/java/mohona_2431026/ViewComplaintStatus.java
@@ -1,35 +1,96 @@
 package mohona_2431026;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.awt.*;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 public class ViewComplaintStatus
 {
-    @javafx.fxml.FXML
+    @FXML
     private TableColumn titleColumn;
-    @javafx.fxml.FXML
+    @FXML
     private TableColumn statusColumn;
-    @javafx.fxml.FXML
+    @FXML
     private TextArea detailsTextArea;
-    @javafx.fxml.FXML
+    @FXML
     private TableView complaintTable;
-    @javafx.fxml.FXML
+    @FXML
     private TableColumn dateColumn;
-    @javafx.fxml.FXML
+    @FXML
     private TableColumn complaintIdColumn;
 
-    @javafx.fxml.FXML
+    @FXML
     public void initialize() {
+
+        complaintIdColumn.setCellValueFactory(new PropertyValueFactory<>("ComplaintID").);
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("complaint title"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("submissiondate"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("current status"));
+
+        loadcomplaints():
+
+        complaintTable.getSelectionModel().selectedItemProperty().addListener(("(observable, oldValue, selectedComplaint) -> {
+        boolean selectedcomplaint;
+
+
+          if  complaintstatusclass selectedComplaint = null;{
+            detailsTextArea.setText(
+                    "Complaint ID : " + selectedComplaint.getComplaintId()
+                            + "\n\nTitle : " + selectedComplaint.getTitle()
+                            + "\n\nDescription : " + selectedComplaint.getDescription()
+                            + "\n\nLocation : " + selectedComplaint.getLocation()
+                            + "\n\nCitizen Name : " + selectedComplaint.getCitizenName()
+                            + "\n\nSubmission Date : " + selectedComplaint.getSubmissionDate()
+                            + "\n\nStatus : " + selectedComplaint.getStatus()
+
+
+            )
+        }
+                ));
     }
 
-    @javafx.fxml.FXML
+    private void loadcomplaints() {
+
+        complaintList.clear();
+        try {
+
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Complaint.bin"));
+
+            while (true) {
+
+                Complaint complaint = (Complaint) ois.readObject();
+                complaintList.add(complaint);
+
+            }
+
+        } catch (EOFException e) {
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        complaintTable.setItems(complaintList);
+
+    }
+    }
+
+    @FXML
     public void refreshButton(ActionEvent actionEvent) {
+
+        loadComplaints();
+
     }
 
-    @javafx.fxml.FXML
+    @FXML
     public void backButton(ActionEvent actionEvent) {
     }
 }
